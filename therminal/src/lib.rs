fab6b2383ae8c192d9b81ae59d26a5513e62b5e1
gglyptodon <janina.mass@gmail.com>
2022-04-20 17:02:53 +0200

    wip

--- therminal/src/lib.rs
+++ therminal/src/lib.rs
@@ -1,18 +1,40 @@
 use clap::{Arg, Command};
 use std::error::Error;
+use std::fmt::{Display, Formatter, write};
 use std::fs::File;
 use std::io::{BufRead, Read};
+use std::thread::sleep;
+use std::time;
+use walkdir::WalkDir;
 
 pub type TherminalResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
-    refresh_rate: usize, //seconds
+    refresh_rate: u64, //seconds
+}
+#[derive(Debug)]
+pub struct ThermalInfo {
+    temp: f32,
+    info: String,
+    kind: String,
+}
+
+impl Display for ThermalInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f,"{}C\t{}\t{}", self.temp, self.info, self.kind)
+    }
 }
 
 pub fn run(config: Config) -> TherminalResult<()> {
     println!("{:#?}", config);
-    println!("{}", read_temp_data()?);
+    loop {
+        //println!("{:#?}", read_temp_data()?);
+        for t in read_temp_data()?{
+            println!("{}", t);
+        }
+        sleep(time::Duration::from_secs(config.refresh_rate))
+    }
     Ok(())
 }
 
@@ -27,22 +49,33 @@ pub fn parse_args() -> TherminalResult<Config> {
                 .help("read sensor values again after SEC seconds"),
         )
         .get_matches();
-    let refresh_rate: usize = matches
+    let refresh_rate = matches
         .value_of("refresh_rate")
         .unwrap_or_default()
-        .parse::<usize>()?;
+        .parse::<u64>()?;
     Ok(Config { refresh_rate })
 }
 
-
-fn read_temp_data() -> TherminalResult<f32>{
-    let file_contents:String = read_file_to_string("/sys/class/thermal/thermal_zone0/temp")?.split_whitespace().collect();
+fn read_temp_data() -> TherminalResult<Vec<ThermalInfo>> {
+    let mut results :Vec<ThermalInfo>= Vec::new();
+    let sensor_files = get_available_temp_sensors()?;
+    for sf in sensor_files{
+       let file_contents: String = read_file_to_string(&sf)? //"/sys/class/thermal/thermal_zone0/temp")?
+        .split_whitespace()
+        .collect();
     let raw_value = file_contents.parse::<usize>()?;
-    let result = raw_value as f32/1000.0;
-    Ok(result)
+    let value_in_celsius = raw_value as f32 / 1000.0;
+        results.push(ThermalInfo{
+            temp: value_in_celsius,
+            info: sf,
+            kind: "TODO".to_string()
+        })
+    }
+
+    Ok(results)
 }
 
-fn read_file_to_string(path: &str)-> TherminalResult<String>{
+fn read_file_to_string(path: &str) -> TherminalResult<String> {
     let mut result_buff = String::new();
     let mut reader = open(path)?;
     reader.read_to_string(&mut result_buff)?;
@@ -54,4 +87,26 @@ pub fn open(filename: &str) -> TherminalResult<Box<dyn BufRead>> {
         "-" => Ok(Box::new(std::io::BufReader::new(std::io::stdin()))),
         _ => Ok(Box::new(std::io::BufReader::new(File::open(filename)?))),
     }
+}
+
+pub fn get_available_temp_sensors() -> TherminalResult<Vec<String>> {
+    let to_try = [
+        "/sys/class/thermal/thermal_zone0/temp",
+        "/sys/class/thermal/thermal_zone1/temp",
+        "/sys/class/hwmon/",
+    ];
+    let mut results:Vec<String> = Vec::new();
+
+    for path in to_try {
+        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            let file_name = entry.file_name().to_string_lossy();
+            match &*file_name {
+             "temp" =>   results.push(entry.path().to_string_lossy().parse().unwrap()),//println!("thermal: {}", file_name),
+             "temp1_input" => results.push(entry.path().to_string_lossy().parse().unwrap()), // println!("hwmon: {}", file_name),
+                _ => {}
+            }
+        }
+    }
+
+    Ok(results)
 }
