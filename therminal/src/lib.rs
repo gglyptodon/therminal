7d1d7e38503d76f0b03e95e252d0249b7ac674c2
gglyptodon <janina.mass@gmail.com>
2022-04-21 11:30:33 +0200

    reading basics

--- therminal/src/lib.rs
+++ therminal/src/lib.rs
@@ -1,10 +1,14 @@
 use clap::{Arg, Command};
+use regex::Regex;
 use std::error::Error;
-use std::fmt::{Display, Formatter, write};
+use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::{BufRead, Read};
 use std::thread::sleep;
 use std::time;
+use std::time::SystemTime;
+use chrono::Utc;
+use walkdir::DirEntry;
 use walkdir::WalkDir;
 
 pub type TherminalResult<T> = Result<T, Box<dyn Error>>;
@@ -22,20 +26,24 @@ pub struct ThermalInfo {
 
 impl Display for ThermalInfo {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f,"{}C\t{}\t{}", self.temp, self.info, self.kind)
+        write!(f, "{}C\t{}\t{}", self.temp, self.info, self.kind)
     }
 }
 
 pub fn run(config: Config) -> TherminalResult<()> {
-    println!("{:#?}", config);
+    //println!("{:#?}", config);
+
     loop {
+        println!("\t# {} #", Utc::now().to_rfc2822());
+
         //println!("{:#?}", read_temp_data()?);
-        for t in read_temp_data()?{
+        for t in read_temp_data()? {
             println!("{}", t);
         }
-        sleep(time::Duration::from_secs(config.refresh_rate))
+        sleep(time::Duration::from_secs(config.refresh_rate));
     }
-    Ok(())
+
+    // Ok(())
 }
 
 pub fn parse_args() -> TherminalResult<Config> {
@@ -57,19 +65,19 @@ pub fn parse_args() -> TherminalResult<Config> {
 }
 
 fn read_temp_data() -> TherminalResult<Vec<ThermalInfo>> {
-    let mut results :Vec<ThermalInfo>= Vec::new();
+    let mut results: Vec<ThermalInfo> = Vec::new();
     let sensor_files = get_available_temp_sensors()?;
-    for sf in sensor_files{
-       let file_contents: String = read_file_to_string(&sf)? //"/sys/class/thermal/thermal_zone0/temp")?
-        .split_whitespace()
-        .collect();
-    let raw_value = file_contents.parse::<usize>()?;
-    let value_in_celsius = raw_value as f32 / 1000.0;
-        results.push(ThermalInfo{
-            temp: value_in_celsius,
-            info: sf,
-            kind: "TODO".to_string()
-        })
+    for sf in sensor_files {
+        if let Ok(res) = read_file_to_string(&sf) {
+            let file_contents: String = res.split_whitespace().collect();
+            let raw_value = file_contents.parse::<usize>()?;
+            let value_in_celsius = raw_value as f32 / 1000.0;
+            results.push(ThermalInfo {
+                temp: value_in_celsius,
+                info: sf,
+                kind: "TODO".to_string(),
+            })
+        }
     }
 
     Ok(results)
@@ -77,8 +85,12 @@ fn read_temp_data() -> TherminalResult<Vec<ThermalInfo>> {
 
 fn read_file_to_string(path: &str) -> TherminalResult<String> {
     let mut result_buff = String::new();
-    let mut reader = open(path)?;
+    let mut reader = open(path).map_err(|e| {
+        eprintln!("path: {}, {}", path, e);
+        e
+    })?;
     reader.read_to_string(&mut result_buff)?;
+
     Ok(result_buff)
 }
 
@@ -90,21 +102,31 @@ pub fn open(filename: &str) -> TherminalResult<Box<dyn BufRead>> {
 }
 
 pub fn get_available_temp_sensors() -> TherminalResult<Vec<String>> {
-    let to_try = [
-        "/sys/class/thermal/thermal_zone0/temp",
-        "/sys/class/thermal/thermal_zone1/temp",
-        "/sys/class/hwmon/",
-    ];
-    let mut results:Vec<String> = Vec::new();
+    let interesting = [Regex::new(r"^temp$")?, Regex::new(r"temp.*_input")?];
+    //let only_files = |entry: &DirEntry| entry.file_type().is_file();
+
+    let name_filter = |entry: &DirEntry| {
+        interesting.is_empty()
+            || interesting
+                .iter()
+                .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+    };
+
+    let to_try = ["/sys/class/thermal/", "/sys/class/hwmon/"];
+    let mut results: Vec<String> = Vec::new();
 
     for path in to_try {
-        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-            let file_name = entry.file_name().to_string_lossy();
-            match &*file_name {
-             "temp" =>   results.push(entry.path().to_string_lossy().parse().unwrap()),//println!("thermal: {}", file_name),
-             "temp1_input" => results.push(entry.path().to_string_lossy().parse().unwrap()), // println!("hwmon: {}", file_name),
-                _ => {}
-            }
+        let entries = walkdir::WalkDir::new(path)
+            .max_depth(2)
+            .follow_links(true)
+            .into_iter()
+            .filter_map(|e|// match e {
+            e.ok())
+            .filter(name_filter)
+            .map(|direntry| direntry.path().display().to_string())
+            .collect::<Vec<_>>();
+        for e in entries {
+            results.push(e)
         }
     }
 
