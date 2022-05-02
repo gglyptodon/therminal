87339beb9e7e2bd86ff42276a4f2a39f7a4aced9
gglyptodon <janina.mass@gmail.com>
2022-05-02 14:49:40 +0200

    add tui

--- therminal/src/lib.rs
+++ therminal/src/lib.rs
@@ -1,13 +1,24 @@
 use chrono::Utc;
 use clap::{Arg, Command};
+
+use cursive::align::HAlign;
+use cursive::event::{Event, Key::Esc};
+use cursive::view::{Nameable, Resizable};
+use cursive::{Cursive, CursiveExt};
+use cursive_table_view::{TableView, TableViewItem};
+use cursive::views::{Dialog};
+
 use regex::Regex;
+use walkdir::DirEntry;
+
+use std::cmp::Ordering;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::{BufRead, Read};
 use std::thread::sleep;
 use std::time;
-use walkdir::DirEntry;
+use std::time::{SystemTime};
 
 pub type TherminalResult<T> = Result<T, Box<dyn Error>>;
 
@@ -16,8 +27,9 @@ pub struct Config {
     refresh_rate: u64, //seconds
     threshold: Option<f32>,
     sensor_id: Option<String>,
+    with_tui: bool,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ThermalInfo {
     temp: f32,
     info: String,
@@ -53,22 +65,26 @@ pub fn run(config: Config) -> TherminalResult<()> {
             ""
         }
     };
-
-    loop {
-        println!("\t# {} #", Utc::now().to_rfc2822());
-        for t in read_temp_data()? {
-            match config.clone().sensor_id {
-                Some(sensor) => {
-                    if t.id.ends_with(&*sensor) {
+    if config.with_tui {
+        tui(&config)?;
+        Ok(())
+    } else {
+        loop {
+            println!("\t# {} #", Utc::now().to_rfc2822());
+            for t in read_temp_data()? {
+                match config.clone().sensor_id {
+                    Some(sensor) => {
+                        if t.id.ends_with(&*sensor) {
+                            println!("{}\t{}\t{}", t.id, t.temp, show_with_threshold(&t))
+                        }
+                    }
+                    _ => {
                         println!("{}\t{}\t{}", t.id, t.temp, show_with_threshold(&t))
                     }
                 }
-                _ => {
-                    println!("{}\t{}\t{}", t.id, t.temp, show_with_threshold(&t))
-                }
             }
+            sleep(time::Duration::from_secs(config.refresh_rate));
         }
-        sleep(time::Duration::from_secs(config.refresh_rate));
     }
 }
 
@@ -98,6 +114,13 @@ pub fn parse_args() -> TherminalResult<Config> {
                 .takes_value(true)
                 .required(false),
         )
+        .arg(
+            Arg::new("tui")
+                .long("tui")
+                .takes_value(false)
+                .required(false)
+                .help("Run with UI"),
+        )
         .get_matches();
     let refresh_rate = matches
         .value_of("refresh_rate")
@@ -107,15 +130,43 @@ pub fn parse_args() -> TherminalResult<Config> {
         Some(t) => Some(t.parse::<f32>()?),
         None => None,
     };
-
+    let with_tui = matches.is_present("tui");
     let sensor_id = matches.value_of("sensor_id").map(String::from);
     Ok(Config {
         refresh_rate,
         threshold,
         sensor_id,
+        with_tui,
     })
 }
 
+fn read_temp_data_after(
+    last_updated: &SystemTime,
+    after_seconds: core::time::Duration,
+) -> TherminalResult<Option<Vec<ThermalInfo>>> {
+    let mut results = None;
+    if SystemTime::now().duration_since(*last_updated)?.as_secs() > after_seconds.as_secs() {
+        //todo
+        let mut readings: Vec<ThermalInfo> = Vec::new();
+        let sensor_files = get_available_temp_sensors()?;
+        for sf in sensor_files {
+            if let Ok(res) = read_file_to_string(&sf) {
+                let file_contents: String = res.split_whitespace().collect();
+                let raw_value = file_contents.parse::<usize>()?;
+                let value_in_celsius = raw_value as f32 / 1000.0;
+                readings.push(ThermalInfo {
+                    temp: value_in_celsius,
+                    info: sf.clone(),
+                    kind: "TODO".to_string(),
+                    id: sf.clone(),
+                })
+            }
+        }
+        results = Some(readings);
+    }
+
+    Ok(results)
+}
 fn read_temp_data() -> TherminalResult<Vec<ThermalInfo>> {
     let mut results: Vec<ThermalInfo> = Vec::new();
     let sensor_files = get_available_temp_sensors()?;
@@ -173,8 +224,7 @@ pub fn get_available_temp_sensors() -> TherminalResult<Vec<String>> {
             .max_depth(2)
             .follow_links(true)
             .into_iter()
-            .filter_map(|e|// match e {
-            e.ok())
+            .filter_map(|e| e.ok())
             .filter(name_filter)
             .map(|direntry| direntry.path().display().to_string())
             .collect::<Vec<_>>();
@@ -185,3 +235,98 @@ pub fn get_available_temp_sensors() -> TherminalResult<Vec<String>> {
 
     Ok(results)
 }
+
+// --- TUI --- \\
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub enum ThermalInfoColumn {
+    Info,
+    Temp,
+    Id,
+}
+
+impl TableViewItem<ThermalInfoColumn> for ThermalInfo {
+    fn to_column(&self, column: ThermalInfoColumn) -> String {
+        match column {
+            ThermalInfoColumn::Info => self.info.to_string(),
+            ThermalInfoColumn::Temp => format!("{}", self.temp),
+            ThermalInfoColumn::Id => self.id.to_string(),
+        }
+    }
+
+    fn cmp(&self, other: &Self, column: ThermalInfoColumn) -> Ordering
+    where
+        Self: Sized,
+    {
+        match column {
+            ThermalInfoColumn::Info => self.info.cmp(&other.info),
+            ThermalInfoColumn::Temp => self.temp.partial_cmp(&other.temp).unwrap(),
+            ThermalInfoColumn::Id => self.id.cmp(&other.id),
+        }
+    }
+}
+fn tui(config: &Config) -> TherminalResult<()> {
+    let refresh_interval = config.refresh_rate;
+    let items = read_temp_data()?;
+    let mut last_updated = SystemTime::now();
+
+    let mut siv = Cursive::new();
+    siv.add_global_callback('q', |s| s.quit());
+    siv.add_global_callback(Esc, |s| s.quit());
+    siv.add_global_callback('u', |s| {
+        s.call_on_name(
+            "table",
+            move |table: &mut TableView<ThermalInfo, ThermalInfoColumn>| {
+                {
+                    table.set_items(read_temp_data().unwrap())
+                }
+            },
+        )
+        .unwrap()
+    });
+
+    siv.add_global_callback(Event::Refresh, move |s| {
+        sleep(time::Duration::from_millis(200));
+        s.call_on_name(
+            "table",
+            |table: &mut TableView<ThermalInfo, ThermalInfoColumn>| {
+                if let Some(readings) = read_temp_data_after(
+                    &last_updated,
+                    core::time::Duration::new(refresh_interval, 0),
+                )
+                .unwrap()
+                {
+                    table.set_items(readings);
+                    last_updated = SystemTime::now();
+                }
+            },
+        )
+        .unwrap()
+    });
+    siv.set_autorefresh(true);
+
+    let mut table = TableView::<ThermalInfo, ThermalInfoColumn>::new()
+        .column(ThermalInfoColumn::Info, "Info", |c| c.width_percent(55))
+        .column(ThermalInfoColumn::Temp, "Temp (°C)", |c| {
+            c.align(HAlign::Center).width_percent(15)
+        })
+        .column(ThermalInfoColumn::Id, "ID", |c| {
+            c.ordering(Ordering::Greater).align(HAlign::Right)
+        });
+    table.set_items(items);
+
+    siv.add_layer(
+        Dialog::around(
+            table
+                .with_name("table")
+                .max_size((800, 600))
+                .min_size((80, 60)),
+        )
+        .title("Thermal Info"),
+    );
+    siv.run();
+    Ok(())
+}
+
+//---------------
+#[cfg(test)]
+mod tests {}
